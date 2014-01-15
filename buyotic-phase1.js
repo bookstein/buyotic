66d8242b93fe791bcfe96b04cecd27c8f7c56cde
emilyboo <ehbookstein@gmail.com>
2014-01-14 23:28:53 -0800

    Remove extra seeLocations, rename MeatChoice, URL cleanup

--- buyotic-phase1.js
+++ buyotic-phase1.js
@@ -1,21 +1,18 @@
 $(document).ready(function() {
 
 	// define a class to create "meat choices" objects
-	function meatChoice (meat) {
+	function MeatChoice (meat) {
 		this.meat = meat;
 	};
 
-	function seeLocations (meat) {
-		console.log("You will be taken to the page for this meat!");
+	function seeLocations(choice) {
+		window.location.href = "http://www.google.com/?q="+choice.meat;
 	};
 
 	$("button").click(function () {
 		var btnValue = $(this).attr("value");
-		var choice = new meatChoice(btnValue);
+		var choice = new MeatChoice(btnValue);
 		seeLocations(choice);
-		function seeLocations(meat) {
-			window.location.href = "www.google.com/"+meat.meat;
-		};
 	})
 
 
