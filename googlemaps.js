9eefd473de386178178b848e4686108362c59612
emilyboo <ehbookstein@gmail.com>
2014-02-11 18:52:17 -0800

    Delete redundant parts of code based on wikiHow google-geocode article, separate geocoderRequest from callback function

--- googlemaps.js
+++ googlemaps.js
@@ -5,12 +5,6 @@ var myGoogleMapAPI = (function() {
 
   function initializeMap() {
     geocoder = new google.maps.Geocoder();
-    var latLong = new google.maps.LatLng(); //what goes in the parentheses????
-    var zip = $("#zip").val(); // set zip code equal to user zip code input!!!
-    var mapOptions = {
-      center: latLong,
-      zoom: 8
-    };
 
     map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
   };
@@ -19,8 +13,10 @@ var myGoogleMapAPI = (function() {
   function geocodeAddress() {
       var address = $("#zip").val(); // use zip value from user input!!!
       var geocodeRequest = {
-        address: address,
-        callback: function(results, status) {
+        address: address
+      };
+
+      var resultsCallback = (function (results, status) {
           if (status == google.maps.GeocoderStatus.OK) {
             map.setCenter(results[0].geometry.location);
             var marker = new google.maps.Marker({
@@ -32,9 +28,9 @@ var myGoogleMapAPI = (function() {
           else {
             alert("We're sorry, we couldn't process your address: " + status);
           }
-        }
-      };
-      geocoder.geocode(geocodeRequest);
+        });
+
+      geocoder.geocode(geocodeRequest, resultsCallback);
   };
 
   // adds <script> to HTML only after page has loaded, uses callback to call initialize()
