5000819015a54a9ecc9725d6a4dbc57e4a1a56c4
emilyboo <ehbookstein@gmail.com>
2014-02-11 17:20:37 -0800

    Use code from google maps API tutorial to asynchronously load google maps - initializes maps after page load

--- googlemaps.js
+++ googlemaps.js
@@ -1,9 +1,20 @@
-      function initialize() {
-        var mapOptions = {
-          center: new google.maps.LatLng(-34.397, 150.644),
-          zoom: 8
-        };
-        var map = new google.maps.Map(document.getElementById("map-canvas"),
-            mapOptions);
-      }
-      google.maps.event.addDomListener(window, 'load', initialize);
+function initialize() {
+  var mapOptions = {
+    center: new google.maps.LatLng(-34.397, 150.644),
+    zoom: 8
+};
+
+var map = new google.maps.Map(document.getElementById("map-canvas"),
+      mapOptions);
+};
+
+// adds <script> to HTML only after page has loaded, uses callback to call initialize()
+function loadScript() {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCcAyejmYX9-FWjezJw1Ywh-8tOXV_sFFg&sensor=true' +
+      'callback=initialize';
+  document.body.appendChild(script);
+};
+
+window.onload = loadScript;
