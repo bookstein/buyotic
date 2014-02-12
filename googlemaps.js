29f1e08529b00c0c3f7a3bb42c30d57eb536476b
emilyboo <ehbookstein@gmail.com>
2014-02-11 18:42:10 -0800

    Playing with namespacing on geocodeRequest and init function

--- googlemaps.js
+++ googlemaps.js
@@ -3,26 +3,24 @@ var myGoogleMapAPI = (function() {
   var geocoder;
   var map;
 
-  function initialize() {
-    //need asynchronous request and callback for Geocoding service
+  function initializeMap() {
+    geocoder = new google.maps.Geocoder();
+    var latLong = new google.maps.LatLng(); //what goes in the parentheses????
+    var zip = $("#zip").val(); // set zip code equal to user zip code input!!!
+    var mapOptions = {
+      center: latLong,
+      zoom: 8
+    };
 
-      geocoder = new google.maps.Geocoder();
-      var latLong; //??
-      var zip = $("#zip").val(); // set zip code equal to user zip code input!!!
-      var mapOptions = {
-        center: latLong,
-        zoom: 8
-      };
-
-      map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
   };
 
 
   function geocodeAddress() {
       var address = $("#zip").val(); // use zip value from user input!!!
-      var geocodeConfig = {
+      var geocodeRequest = {
         address: address,
-        geocodeRequest: function(results, status) {
+        callback: function(results, status) {
           if (status == google.maps.GeocoderStatus.OK) {
             map.setCenter(results[0].geometry.location);
             var marker = new google.maps.Marker({
@@ -32,11 +30,11 @@ var myGoogleMapAPI = (function() {
           }
 
           else {
-            alert("We're sorry, we couldn't process your input: " + status);
+            alert("We're sorry, we couldn't process your address: " + status);
           }
         }
       };
-      geocoder.geocode(geocodeConfig);
+      geocoder.geocode(geocodeRequest);
   };
 
   // adds <script> to HTML only after page has loaded, uses callback to call initialize()
@@ -44,7 +42,7 @@ var myGoogleMapAPI = (function() {
     var script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCcAyejmYX9-FWjezJw1Ywh-8tOXV_sFFg&sensor=true' +
-        'callback=initialize';
+        'callback=initializeMap';
     document.body.appendChild(script);
   };
 
