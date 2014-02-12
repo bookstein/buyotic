6107ab65d5136ed4a8bfd7b05c48c2a7c455c9ee
emilyboo <ehbookstein@gmail.com>
2014-02-11 18:22:48 -0800

    write geocodeAddress function using googlemapAPI docs example under Status Codes. wrap all functions inside global variable

--- googlemaps.js
+++ googlemaps.js
@@ -1,63 +1,53 @@
-function initialize() {
-  //need asynchronous request and callback for Geocoding service
-  initializeGeocoder();
+var myGoogleMapAPI = (function() {
 
+  var geocoder;
+  var map;
 
-  var mapOptions = {
-    center: // center of requested zip code
-    zoom: 8
-};
+  function initialize() {
+    //need asynchronous request and callback for Geocoding service
 
-function initializeGeocoder () {
-  var geocoder = new google.maps.Geocoder();
-  console.log(geocoder);
-  var zip = $("#zip").val(); // set zip code equal to user zip code input!!!
-  var geocodeRequest = {
-    address: zip,
-    getGeocode: function(result, status) {
+      geocoder = new google.maps.Geocoder();
+      var latLong; //??
+      var zip = $("#zip").val(); // set zip code equal to user zip code input!!!
+      var mapOptions = {
+        center: latLong,
+        zoom: 8
+      };
 
-    }
+      map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
   };
 
 
-// if there's a geocoding error
-var reasons=[];
+  function geocodeAddress() {
+      var address = $("#zip").val(); // use zip value from user input!!!
+      var geocodeConfig = {
+        address: address,
+        geocodeRequest: function(results, status) {
+          if (status == google.maps.GeocoderStatus.OK) {
+            map.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location
+            });
+          }
 
-reasons[G_GEO_SUCCESS]            = "Success";
+          else {
+            alert("We're sorry, we couldn't process your input: " + status);
+          }
+        }
+      };
+      geocoder.geocode(geocodeConfig);
+  };
 
-reasons[G_GEO_MISSING_ADDRESS]    = "Missing Address: The address was either missing or had
-                                    no value.";
+  // adds <script> to HTML only after page has loaded, uses callback to call initialize()
+  function loadScript() {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCcAyejmYX9-FWjezJw1Ywh-8tOXV_sFFg&sensor=true' +
+        'callback=initialize';
+    document.body.appendChild(script);
+  };
 
-reasons[G_GEO_UNKNOWN_ADDRESS]    = "Unknown Address:  No corresponding geographic location
-                                    could be found for the specified address.";
+  window.onload = loadScript;
 
-reasons[G_GEO_UNAVAILABLE_ADDRESS]= "Unavailable Address:  The geocode for the given address
-                                    cannot be returned due to legal or contractual reasons.";
-
-reasons[G_GEO_BAD_KEY]            = "Bad Key: The API key is either invalid or does not match
-                                    the domain for which it was given";
-
-reasons[G_GEO_TOO_MANY_QUERIES]   = "Too Many Queries: The daily geocoding quota for this site
-                                    has been exceeded.";
-
-reasons[G_GEO_SERVER_ERROR]       = "Server error: The geocoding request could not be
-                                      successfully processed.";
-
-
-  geocoder.geocode(geocodeRequest);
-}
-
-var map = new google.maps.Map(document.getElementById("map-canvas"),
-      mapOptions);
-};
-
-// adds <script> to HTML only after page has loaded, uses callback to call initialize()
-function loadScript() {
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCcAyejmYX9-FWjezJw1Ywh-8tOXV_sFFg&sensor=true' +
-      'callback=initialize';
-  document.body.appendChild(script);
-};
-
-window.onload = loadScript;
+})();
