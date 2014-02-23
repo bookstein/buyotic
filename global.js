70c5560b094aa2e3297da9c6de5fb158908b1b71
emilyboo <ehbookstein@gmail.com>
2014-02-22 21:43:13 -0800

    Remove testing method

--- global.js
+++ global.js
@@ -3,8 +3,6 @@ var buyotic = {
 	hideOrShowElements: function hideOrShowElems (hideElement, showElement) {
 		$(showElement).css("display", "block");
 		$(hideElement).css("display", "none")
-	},
-
-	testMethod: alert("Testing!")
+	}
 
 };
