70da99e92e03a4618e99309fa3188fb3a3b00e9c
emilyboo <ehbookstein@gmail.com>
2014-02-22 21:30:28 -0800

    Find.js contains find-meat specific functions, copied from buyotic-phase1.js. Global.js holds functions that apply across pages.

---
+++ find.js
@@ -0,0 +1,74 @@
+$(document).ready(function () {
+
+	var findMeat = (function() {
+		//View
+
+		//Model
+		// user-submitted data
+			function MeatChoice (checkedMeat, checkedOptions, location) {
+				this.checkedMeat = checkedMeat;
+				this.organic = checkedOptions.organic;
+				this.local = checkedOptions.local;
+				this.lean = checkedOptions.lean;
+				this.freeRange = checkedOptions.freeRange;
+				this.zip = $("#zip").val();
+				this.radius = $("#radius").val();
+			};
+
+		/*hash map
+			var checkedOptionsObj = {
+				organic: 0,
+				local: 1,
+				lean: 2,
+				freeRange: 3
+			};*/
+
+			for (property in checkedOptionsObj) {
+
+				if($("#" + property).find("input").is(":checked")) {
+					checkedOptionsObj[property] = true;
+				}
+				else {
+					checkedOptionsObj[property] = false;
+				}
+			}
+
+			// creates an object containing checked options
+
+				var checkedOptions = {};
+				$("#search-options").find("input:checkbox:checked").each(function () {
+					var property = $(this).val();
+					checkedOptions[property] = true;
+					return checkedOptions;
+				});
+
+				for (var i = 0; i <= checkedMeat.length; i++) {
+					if (checkedMeat[i]) {
+						var meat = checkedMeat[i];
+						console.log(meat);
+						meat = new MeatChoice(meat, checkedOptions);
+						console.log(meat);
+					}
+				}
+
+		// Controller
+			function submitMeatSearch () {
+				$("#submit-meat-search").on("click", function() {
+					var checkedMeat = [];
+					$("#meat-types").find("input:checkbox:checked").each(function () {
+						checkedMeat.push($(this).val());
+						return checkedMeat;
+					});
+				});
+			};
+
+			});
+	})();
+
+	// User submits data about meat in local area (crowdsourcing)
+		//
+		//
+		// use node (or MySQL) to interact with Firebase or MongoDB (backend)
+		// node = do it all in JS!
+		// npm can be used to install Mongo or SQL
+});
---
+++ global.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+	var buyotic = (function () {
+
+		function hideOrRevealElems (hideElement, showElement) {
+
+					$(showElement).css("display", "block");
+					$(hideElement).css("display", "none")
+		}
+
+	})();
+
+});
